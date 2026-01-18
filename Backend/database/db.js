c2cc4d93e1b69e53b7e82c720b7a1009a57d13a6
Aswinbabu6233 <aswinbabu6933@gmail.com>
2026-01-18 15:54:11 +0530

    feat: Initialize backend MongoDB connection and configure `.gitignore` files for both backend and frontend.

---
+++ Backend/database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB database EchoTix");
+  console.log("");
+});
+
+module.exports = db;
